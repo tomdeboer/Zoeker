cb1a30e24fa6f6a007daceb391938bea7fedb062
Tom Boer <tom@tomdeboer.nl>
2016-04-11 23:34:18 +0200

    Layout tweaks

--- js/recording.model.js
+++ js/recording.model.js
@@ -35,7 +35,7 @@ var RecordingModel = Backbone.Model.extend({
 			var start = new Date(buf.readUInt32LE(0x280) * 1000);
 			var stop  = new Date(buf.readUInt32LE(0x284) * 1000);
 			var channelName = buf.toString("utf-8", 913, 913+31);
-			var programName = buf.toString("utf-8", 666, 666+47);
+			var programName = buf.toString("utf-8", 666, 666+49);
 			var description = buf.toString("binary", 1343, 1343+255);
 
 			self.set('dateStart', start);
